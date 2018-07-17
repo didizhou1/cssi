e06d1e9854c9c3fc4b3ca1b0a9cbe325cce405bb
Demo User <demouser@MacBook-Pro.local>
2018-07-17 15:42:22 -0500

    javascript practice

---
+++ javaScript/javascript.js
@@ -0,0 +1,21 @@
+console.log('Hello World');
+
+const name="Charlie";
+console.log("Hello "+name);
+//console.log('Hello ${name}!');
+
+const age=16;
+console.log("You are "+age+" years old");
+
+// line comment
+/* block comment */
+/** documentation */
+if(age>=18){
+  console.log("You can get your driver's license and vote")
+}
+else if (age>=15){
+  console.log("You can get your permit but you cannot vote")
+}
+else{
+  console.log("You will have to wait to get your permit and cannot vote")
+}
